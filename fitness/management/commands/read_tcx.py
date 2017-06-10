a96b1566a2d884caf18cfaf6e8da2479ccd9e1e7
Karl Odie <karlodie@gmail.com>
2017-06-10 06:56:29 +0100

    TCX logic on to command

--- fitness/management/commands/read_tcx.py
+++ fitness/management/commands/read_tcx.py
@@ -1,6 +1,14 @@
 import glob
-from django.core.management.base import BaseCommand, CommandError
-from fitness.models import read_tcx
+import xml.etree.ElementTree as ET
+import dateutil.parser
+from django.core.management.base import BaseCommand
+
+from fitness.models import Activity, Lap, Point
+
+NAMESPACES = {
+    'default': 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2',
+    'extension': 'http://www.garmin.com/xmlschemas/ActivityExtension/v2',
+}
 
 
 class Command(BaseCommand):
@@ -17,3 +25,49 @@ class Command(BaseCommand):
                     self.stdout.write(self.style.SUCCESS(
                         'Added {} {}'.format(activity.name, activity.time)
                     ))
+
+
+def read_tcx(filename):
+    tree = ET.parse(filename)
+    tcx = tree.getroot()
+    activities = []
+    for activity_list in tcx.findall('default:Activities', NAMESPACES):
+        for xml_activity in activity_list.findall('default:Activity', NAMESPACES):
+            name = xml_activity.find('default:Notes', NAMESPACES).text
+            start = dateutil.parser.parse(xml_activity.find('default:Id', NAMESPACES).text)
+            if Activity.objects.filter(time=start):
+                continue
+            activity = Activity(name=name, time=start)
+            activity.save()
+            activities.append(activity)
+            for index, xml_lap in enumerate(xml_activity.findall('default:Lap', NAMESPACES), start=1):
+                lap = Lap(activity=activity, lap=index)
+                lap.save()
+                for xml_track in xml_lap.findall('default:Track', NAMESPACES):
+                    for xml_point in xml_track.findall('default:Trackpoint', NAMESPACES):
+                        try:
+                            time = dateutil.parser.parse(xml_point.find('default:Time', NAMESPACES).text)
+                            position = xml_point.find('default:Position', NAMESPACES)
+                            latitude = position.find('default:LatitudeDegrees', NAMESPACES).text
+                            longitude = position.find('default:LongitudeDegrees', NAMESPACES).text
+                            altitude = xml_point.find('default:AltitudeMeters', NAMESPACES).text
+                        except AttributeError:
+                            continue
+                        try:
+                            heart_rate = xml_point.find('default:HeartRateBpm', NAMESPACES).find('default:Value', NAMESPACES).text
+                        except AttributeError:
+                            heart_rate = 0
+                        extensions = xml_point.find('default:Extensions', NAMESPACES)
+                        tpx = extensions.find('extension:TPX', NAMESPACES)
+                        cadence = tpx.find('extension:RunCadence', NAMESPACES).text
+                        point = Point(
+                            lap=lap,
+                            time=time,
+                            latitude=latitude,
+                            longitude=longitude,
+                            altitude=altitude,
+                            heart_rate=heart_rate,
+                            cadence=cadence
+                        )
+                        point.save()
+    return activities
--- fitness/models.py
+++ fitness/models.py
@@ -1,7 +1,4 @@
 import math
-import xml.etree.ElementTree as ET
-
-import dateutil.parser
 
 from django.db import models
 
@@ -41,54 +38,3 @@ class Point(models.Model):
     altitude = models.FloatField()
     heart_rate = models.FloatField()
     cadence = models.FloatField()
-
-NAMESPACES = {
-    'default': 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2',
-    'extension': 'http://www.garmin.com/xmlschemas/ActivityExtension/v2',
-}
-
-
-def read_tcx(filename):
-    tree = ET.parse(filename)
-    tcx = tree.getroot()
-    activities = []
-    for activity_list in tcx.findall('default:Activities', NAMESPACES):
-        for xml_activity in activity_list.findall('default:Activity', NAMESPACES):
-            name = xml_activity.find('default:Notes', NAMESPACES).text
-            start = dateutil.parser.parse(xml_activity.find('default:Id', NAMESPACES).text)
-            if Activity.objects.filter(time=start):
-                continue
-            activity = Activity(name=name, time=start)
-            activity.save()
-            activities.append(activity)
-            for index, xml_lap in enumerate(xml_activity.findall('default:Lap', NAMESPACES), start=1):
-                lap = Lap(activity=activity, lap=index)
-                lap.save()
-                for xml_track in xml_lap.findall('default:Track', NAMESPACES):
-                    for xml_point in xml_track.findall('default:Trackpoint', NAMESPACES):
-                        try:
-                            time = dateutil.parser.parse(xml_point.find('default:Time', NAMESPACES).text)
-                            position = xml_point.find('default:Position', NAMESPACES)
-                            latitude = position.find('default:LatitudeDegrees', NAMESPACES).text
-                            longitude = position.find('default:LongitudeDegrees', NAMESPACES).text
-                            altitude = xml_point.find('default:AltitudeMeters', NAMESPACES).text
-                        except AttributeError:
-                            continue
-                        try:
-                            heart_rate = xml_point.find('default:HeartRateBpm', NAMESPACES).find('default:Value', NAMESPACES).text
-                        except AttributeError:
-                            heart_rate = 0
-                        extensions = xml_point.find('default:Extensions', NAMESPACES)
-                        tpx = extensions.find('extension:TPX', NAMESPACES)
-                        cadence = tpx.find('extension:RunCadence', NAMESPACES).text
-                        point = Point(
-                            lap=lap,
-                            time=time,
-                            latitude=latitude,
-                            longitude=longitude,
-                            altitude=altitude,
-                            heart_rate=heart_rate,
-                            cadence=cadence
-                        )
-                        point.save()
-    return activities
