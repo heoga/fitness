3a409545eaf1acdc68734af9f7a1b9422a3155dd
Karl Odie <karlodie@gmail.com>
2017-07-10 20:53:46 +0100

    Tidy up views

--- fitness/views.py
+++ fitness/views.py
@@ -1,5 +1,4 @@
 from django.views.generic import ListView, DetailView
-from rest_framework import viewsets
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse_lazy
 
@@ -8,59 +7,59 @@ from django.views import View
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, CreateModelMixin
 
-from fitness.forms import ProfileForm, HeartRateForm, UserForm
-
-from fitness.models import Activity
-from fitness.serializers import ActivityDetailSerializer, ActivityListSerializer, TrimpSerializer, RunSerializer
+from . import forms
+from . import models
+from . import serializers
 
 
 class ActivityList(LoginRequiredMixin, ListView):
     template_name = 'fitness/activity_list.html'
-    model = Activity
+    model = models.Activity
     paginate_by = 30
 
     def get_queryset(self):
-        return Activity.objects.filter(owner=self.request.user)
+        return models.Activity.objects.filter(owner=self.request.user)
 
 
 class ActivityDetail(LoginRequiredMixin, DetailView):
     template_name = 'fitness/activity_detail.html'
-    model = Activity
+    model = models.Activity
 
     def get_queryset(self):
-        return Activity.objects.filter(owner=self.request.user)
+        return models.Activity.objects.filter(owner=self.request.user)
 
 
 class ActivitySVG(LoginRequiredMixin, DetailView):
     template_name = 'fitness/activity.svg'
-    model = Activity
+    model = models.Activity
 
     def get_queryset(self):
-        return Activity.objects.filter(owner=self.request.user)
+        return models.Activity.objects.filter(owner=self.request.user)
 
 
 class ActivityViewSet(
     RetrieveModelMixin, ListModelMixin, CreateModelMixin, viewsets.GenericViewSet
 ):
     def get_queryset(self):
-        return Activity.objects.filter(owner=self.request.user)
+        return models.Activity.objects.filter(owner=self.request.user)
 
     def get_serializer_class(self):
         return {
-            'retrieve': ActivityDetailSerializer,
-            'create': RunSerializer
-        }.get(self.action) or ActivityListSerializer
+            'retrieve': serializers.ActivityDetailSerializer,
+            'create': serializers.RunSerializer
+        }.get(self.action) or serializers.ActivityListSerializer
 
 
 class TrimpViewSet(viewsets.ViewSet):
-    queryset = Activity.objects.all()
+    queryset = models.Activity.objects.all()
 
     def list(self, request):
-        serializer = TrimpSerializer(
-            Activity.trimp_history(self.request.user),
+        serializer = serializers.TrimpSerializer(
+            models.Activity.trimp_history(self.request.user),
             many=True, context={'request': request}
         )
         return Response(serializer.data)
@@ -71,16 +70,16 @@ class ControlPanelView(LoginRequiredMixin, View):
     template_name = "fitness/control_panel.html"
 
     def get(self, request, *args, **kwargs):
-        user_form = UserForm(initial={
+        user_form = forms.UserForm(initial={
             'first_name': self.request.user.first_name,
             'last_name': self.request.user.last_name,
             'email': self.request.user.email,
         })
-        profile_form = ProfileForm(initial={
+        profile_form = forms.ProfileForm(initial={
             'theme': self.request.user.profile.theme,
             'gender': self.request.user.profile.gender,
         })
-        heart_form = HeartRateForm(initial={
+        heart_form = forms.HeartRateForm(initial={
             'minimum_heart_rate': self.request.user.profile.minimum_heart_rate,
             'maximum_heart_rate': self.request.user.profile.maximum_heart_rate
         })
@@ -92,12 +91,15 @@ class ControlPanelView(LoginRequiredMixin, View):
         })
 
     def post(self, request, *args, **kwargs):
-        user_form = UserForm(request.POST)
-        profile_form = ProfileForm(request.POST)
-        if user_form.is_valid() and profile_form.is_valid():
+        user_form = forms.UserForm(request.POST)
+        profile_form = forms.ProfileForm(request.POST)
+        heart_form = forms.HeartRateForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid() and heart_form.is_valid():
             for key, value in user_form.cleaned_data.items():
                 setattr(self.request.user, key, value)
             for key, value in profile_form.cleaned_data.items():
+                setattr(self.request.user.profile, key, value)
+            for key, value in heart_form.cleaned_data.items():
                 setattr(self.request.user.profile, key, value)
             self.request.user.save()
             self.request.user.profile.save()
@@ -115,7 +117,7 @@ class TrimpView(LoginRequiredMixin, TemplateView):
 class RecalculateTRIMPView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         id = kwargs['pk']
-        activity = Activity.objects.filter(owner=request.user, id=id).first()
+        activity = models.Activity.objects.filter(owner=request.user, id=id).first()
         if activity:
             activity.trimp = activity.calculate_trimp()
             activity.save()
